ba9d0ff993e4e6c856cce3a91ecf8fe38c9c1d55
geonyoung Min <hahaha3456@naver.com>
2023-09-11 06:24:10 +0000

    수정

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,35 +1,54 @@
-/**
- * eslint-disable @sap/ui5-jsdocs/no-jsdoc
- */
-
 sap.ui.define([
-        "sap/ui/core/UIComponent",
-        "sap/ui/Device",
-        "project1/model/models"
-    ],
-    function (UIComponent, Device, models) {
-        "use strict";
+   "sap/ui/core/UIComponent",
+   "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
+   "sap/ui/Device",
+   "project1/model/models"
+],
+function (UIComponent, JSONModel, ResourceModel, Device, models) {
+   "use strict";
 
-        return UIComponent.extend("project1.Component", {
-            metadata: {
-                manifest: "json"
-            },
+   return UIComponent.extend("project1.Component", {
+       metadata: {
+           manifest: "json",
+           "interfaces": ["sap.ui.core.IAsyncContentCreation"],
+           "rootView": {
+            "viewName": "project1.view.App",
+            "type": "XML",
+            /*"async": true, // implicitly set via the sap.ui.core.IAsyncContentCreation interface*/
+            "id": "app"
+         }
+       },
 
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
-            init: function () {
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, arguments);
+       /**
+        * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+        * @public
+        * @override
+        */
+       init: function () {
+           // call the base component's init function
+           UIComponent.prototype.init.apply(this, arguments);
 
-                // enable routing
-                this.getRouter().initialize();
+           // enable routing
+           this.getRouter().initialize();
 
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
-            }
-        });
-    }
+           // set the device model
+           this.setModel(models.createDeviceModel(), "device");
+
+           var oData = {
+                         recipient : {
+                            name : "World"
+                         }
+                      };
+                      var oModel = new JSONModel(oData);
+                      this.setModel(oModel);
+             
+                      var i18nModel = new ResourceModel({
+                         bundleName: "project1.i18n.i18n"
+                      });
+                      this.setModel(i18nModel, "i18n");
+            
+       }
+   });
+}
 );
--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -1,11 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-  ], function (Controller,MessageToast) {
-    "use strict";
-    return Controller.extend("project1.controller.app", {
-      onShowHello: function() {
-        MessageToast.show("Hello World");
-      }
+       "sap/ui/core/mvc/Controller",
+    ], function (Controller) {
+       "use strict";
+       return Controller.extend("project1.controller.app", {
+       });
     });
-  });
+   
