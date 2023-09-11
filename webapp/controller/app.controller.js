6f88e846595186da065c654f1251129fe251109b
geonyoung Min <hahaha3456@naver.com>
2023-09-11 02:58:45 +0000

    수정

--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -1,15 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
-
-        return Controller.extend("project1.controller.app", {
-            onInit: function () {
-
-            }
-        });
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+  ], function (Controller,MessageToast) {
+    "use strict";
+    return Controller.extend("project1.controller.app", {
+      onShowHello: function() {
+        MessageToast.show("Hello World");
+      }
     });
+  });
